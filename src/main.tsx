cecbdba02f45ae9d1903070641fa9efb7e220686
麦子卓 <maizizhuo@example.com>
2026-07-31 20:39:31 +0800

    feat: increase table typography by 15 percent

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "@fontsource/space-grotesk/600.css";
 import "@fontsource/space-grotesk/700.css";
 import App from "./App";
 import "./styles.css";
+import "./styles/table-typography.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
